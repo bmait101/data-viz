9fc52c29ee0860a19d72dc38ee86adfc4ed3b2a3
bmait101 <bmaitland101@gmail.com>
2024-02-09 11:17:43 -0600

    clean up

--- R/fs-budget.R
+++ R/fs-budget.R
@@ -1,5 +1,6 @@
-# Data Vis - US FS Budget
+# Visualizing the US Forest Service Budget
 
+# Load libraries
 library(tidyverse)
 library(readxl)
 library(scales)
@@ -9,9 +10,10 @@ library(ggsci)
 library(magick)
 
 
-
+# Data
 fs_budget <- read_excel("data/fs-budget.xlsx")
 
+# Prep data
 fs_budget <- fs_budget |> 
   pivot_longer(-Appropriations, names_to = "year", values_to = "budget") |> 
   mutate(year = as.numeric(year)) |> 
@@ -19,7 +21,7 @@ fs_budget <- fs_budget |>
                          as.character(Appropriations), 
                          NA_character_))
 
-
+# Build plot
 p1 <- fs_budget |> 
   ggplot(aes(x=year, y=budget, color=Appropriations)) +
   geom_vline(xintercept = 2023, linetype="dashed", color = "grey") +
@@ -30,7 +32,7 @@ p1 <- fs_budget |>
     "text", x = 2024.25, y = 3100000, size = 4,
     label = "Estimates", color = "grey") +
   geom_point(size = 2) +
-  geom_line(size = 1) +
+  geom_line(linewidth = 1) +
   scale_color_lancet() +  
   scale_x_continuous(
     breaks = seq(2011,2024,1),
@@ -80,6 +82,7 @@ p1 <- fs_budget |>
   )
 p1
 
+# Try to add forest servce logo to plot
 # image_read_svg(here::here("img","USFS-logo.png"))
 # logo <- png::readPNG(here::here("img","USFS-logo.png"))
 # image <- image_fill(logo, 'none')
@@ -87,83 +90,8 @@ p1
 # myplot <- qplot(mpg, wt, data = mtcars)
 # myplot + annotation_raster(raster, 25, 35, 3, 5)
 
-
+# Save plot
 ggsave("plots/fs-budget.png",
        width = 5, height = 3, scale = 1.7,
        bg = "white"
 )
-
-
-p2 <- fs_budget |> 
-  ggplot(aes(x=year, y=budget, color=Appropriations)) +
-  geom_vline(xintercept = 2023, linetype="dashed", color = "grey") +
-  annotate(
-    "text", x = 2022.3, y = 520000, size = 4,
-    label = "Actual", color = "grey") +
-  annotate(
-    "text", x = 2024, y = 520000, size = 4,
-    label = "Estimates", color = "grey") +
-  geom_point(size = 2.5) +
-  geom_line(size = 1.25) +
-  scale_color_lancet() +  
-  scale_x_continuous(
-    breaks = seq(2011,2024,1),
-    limits = c(2011,2034), 
-    expand = c(0.01,0)
-  ) +
-  scale_y_continuous(
-    limits = c(0,520000),
-    labels = label_number(scale_cut = cut_short_scale()), 
-    expand = c(0.01,0)
-  ) +
-  labs(
-    x = "", y = "",
-    title = "budget excluding National Forest System and Wildland Fire Management appropriations",
-    subtitle = "U.S. Dollars",
-  ) + 
-  theme_minimal() + 
-  theme(
-    panel.grid.major.x = element_blank(),
-    panel.grid.major.y = element_line(),
-    panel.grid.minor.y = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    axis.ticks.x = element_line(),
-    axis.ticks.length = unit("2", "mm"),
-    plot.title.position = "plot",
-    plot.title = element_text(face="bold"),
-    plot.caption = element_text(hjust = 0), 
-    plot.background = element_rect(fill = "white", color = "black", size = 2),
-    legend.position = "none"
-  ) +
-  geom_text_repel(
-    data = fs_budget_sub |> filter(!is.na(label)),
-    aes(label = paste0("  ", label)),
-    size = 5,
-    segment.curvature = -0.1,
-    segment.square = TRUE,
-    box.padding = 0.1,
-    point.padding = 0.6,
-    nudge_x = 0.15,
-    nudge_y = 1,
-    force = 0.5,
-    hjust = 0,
-    direction="y",
-    na.rm = TRUE, 
-    xlim = c(2025,2034),
-    ylim = c(0,600000)
-  )
-p2
-
-# ggsave("plots/fs-budget-sub.png",
-#        width = 7, height = 4, scale = 1.5, 
-#        bg = "white"
-# )
-
-# p1 + p2 + 
-#   plot_layout(widths = c(1.3, 1))
-# 
-# ggsave("plots/fs-budget-panel.png",
-#        width = 12, height = 4, scale = 1.7, 
-#        bg = "white"
-# )
-  
